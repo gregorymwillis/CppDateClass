7ab9cdc92070c5801e3d9505a095824611e6e558
Greg Willis <gregorymwillis@gmail.com>
2017-10-23 14:53:35 -0400

    Added an array to manage month strings

--- Date.cpp
+++ Date.cpp
@@ -86,35 +86,9 @@ void Date::printFormat3(Date d) {
 }
 
 string Date::getMonthString(Date d) {
-	string month = "";
-
-	switch (d.getMonth()) {
-		case 1: month = "January";
-			break;
-		case 2: month = "February";
-			break;
-		case 3: month = "March";
-			break;
-		case 4: month = "April";
-			break;
-		case 5: month = "May";
-			break;
-		case 6: month = "June";
-			break;
-		case 7: month = "July";
-			break;
-		case 8: month = "August";
-			break;
-		case 9: month = "September";
-			break;
-		case 10: month = "October";
-			break;
-		case 11: month = "November";
-			break;
-		case 12: month = "December";
-			break;
-		default: month = "";
-			break;
-	}
-	return month;
+	int index = d.getMonth() - 1;
+	string months[12] = {"January", "February", "March", "April",
+				"May", "June", "July", "August",
+				"September", "October", "November", "December"};	
+	return months[index];
 } 
